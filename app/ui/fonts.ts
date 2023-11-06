fed3edb425c2871ec555d54e5771d2cbade9ab49
CinLo82 <cinlos3382@gmail.com>
2023-11-06 16:40:52 -0300

    agregamos font, y diferentes maneras de agregar styles

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana, Montserrat } from 'next/font/google'
+
+export const monserrat = Montserrat({ subsets: ['latin'] })
+
+export const lucitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+})
